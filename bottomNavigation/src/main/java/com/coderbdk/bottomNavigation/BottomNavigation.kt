6ca9598015bb5646b6b8f5ff9e1f7ae12eb47429
CoderBdk <cobdgithub07@gmail.com>
2024-12-07 22:02:47 +0600

    Initial commit

---
+++ bottomNavigation/src/main/java/com/coderbdk/bottomNavigation/BottomNavigation.kt
@@ -0,0 +1,10 @@
+package com.coderbdk.bottomNavigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BottomNavigation(modifier: Modifier = Modifier) {
+
+}
